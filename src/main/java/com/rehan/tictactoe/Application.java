47dd0618497417da82dd8c6bcac2a1f19cd60eca
monikaqt75 <monika.chatta@qualitestgroup.com>
2025-10-10 17:52:51 +0530

    Update Application.java

--- src/main/java/com/rehan/tictactoe/Application.java
+++ src/main/java/com/rehan/tictactoe/Application.java
@@ -7,5 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    // used this line for testing:     
+    invalid_syntax_error_here;     
     }
 }
