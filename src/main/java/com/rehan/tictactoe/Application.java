2239a528e2ba267dbce54350298fa09b24156f3e
monikaqt75 <monika.chatta@qualitestgroup.com>
2025-10-13 12:08:32 +0530

    Update Application.java

--- src/main/java/com/rehan/tictactoe/Application.java
+++ src/main/java/com/rehan/tictactoe/Application.java
@@ -8,6 +8,6 @@ public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     // used this line for testing:     
-    invalid_syntax_error_here;     
+    // invalid_syntax_error_here;     
     }
 }
